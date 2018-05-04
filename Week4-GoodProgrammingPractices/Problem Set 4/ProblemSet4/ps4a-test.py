587125ebc42db174e616e0f1fdd02e3588b096e2
xiaoxiaoc111 <fzuxiaoxiaoc@gmail.com>
2018-05-05 01:07:33 +0800

    update problem4

--- Week4-GoodProgrammingPractices/Problem Set 4/ProblemSet4/ps4a-test.py
+++ Week4-GoodProgrammingPractices/Problem Set 4/ProblemSet4/ps4a-test.py
@@ -157,6 +157,12 @@ def updateHand(hand, word):
 #
 # Problem #3: Test word validity
 #
+
+
+hand = {'r': 1, 'a': 3, 'p': 2, 'e': 1, 't': 1, 'u':1}
+word = "rapture"
+wordlist = WORDLIST_FILENAME
+
 def isValidWord(word, hand, wordList):
     """
     Returns True if word is in the wordList and is entirely
@@ -169,7 +175,25 @@ def isValidWord(word, hand, wordList):
     wordList: list of lowercase strings
     """
     # TO DO ... <-- Remove this comment when you code this function
+    output = hand.copy()
+    word_check = False
+    if word in wordList:
+        word_check = True
+ 
+    letter_check = set(list(word)) <= set(output.keys())
+ 
+    for letter in word:
+        if letter in output.keys():
+            output[letter] -= 1
+ 
+    value_check = all(i >= 0 for i in output.values())
+    
+    if word_check == True and letter_check == True and value_check == True:
+        return True
+    else:
+        return False
 
+      
 
 #
 # Problem #4: Playing a hand
